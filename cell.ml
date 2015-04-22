5c4521bd9bc031b7c1c96d848a14a13b865a815a
Michael Rose <michaelrose93@gmail.com>
2015-04-21 20:49:53 -0400

    Added inputs to the cell object
    Thought we might want to feed a new cell certain starting values for
    age/color/status; cell.ml now has 2 objects, a set “dead_cell” with age
    of 0, color white, and Dead status, and a cell object that can take
    inputs for those values instead.

--- cell.ml
+++ cell.ml
@@ -1,6 +1,28 @@
 type status = Alive | Dead
 
-class cell = 
+class cell (a : int) (st : status) c = 
+object
+
+	(* Starting age, 0 *)
+	val mutable age = a
+
+	(* Starting status, dead *)
+	val mutable status = st
+
+	(* Starting color, white - should check that this will actually work *)
+	val mutable color = c
+
+	(* Updates the color to the value c *)
+	method update_color c = color <- c 
+
+	(* Kills or resurrects the cell *)
+	method change_state s = status <- s
+
+	(* Ages the cell by a specific amount *)
+	method age i = age <- age + i
+end
+
+class dead_cell = 
 object
 
 	(* Starting age, 0 *)
