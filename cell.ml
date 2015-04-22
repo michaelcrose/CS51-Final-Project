3920801e615d4c4e8c72d20ee85914451efccb63
Michael Rose <michaelrose93@gmail.com>
2015-04-21 20:43:11 -0400

    Comments
    Added comments

--- cell.ml
+++ cell.ml
@@ -3,15 +3,21 @@ type status = Alive | Dead
 class cell = 
 object
 
+	(* Starting age, 0 *)
 	val mutable age = 0
 
+	(* Starting status, dead *)
 	val mutable status = Dead
 
+	(* Starting color, white - should check that this will actually work *)
 	val mutable color = Graphics.white
 
+	(* Updates the color to the value c *)
 	method update_color c = color <- c 
 
+	(* Kills or resurrects the cell *)
 	method change_state s = status <- s
 
+	(* Ages the cell by a specific amount *)
 	method age i = age <- age + i
 end
